55e5d87026def1cc3013ddb07376fd19417e5769
Gianni Abarca <jefemaestroasf@gmail.com>
2024-06-22 20:53:57 -0500

    Logrado el login

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
+// const express = require('express');
+// const app = express();
+// const usuarioRoutes = require('./routes/usuarioRoutes');
+// const authRoutes = require('./routes/authRoutes'); // Importa el archivo de rutas de autenticación
+// const path = require('path'); // Importar el módulo 'path'
+
+// // Middleware para manejar JSON en las solicitudes
+// app.use(express.json());
+
+// // Middleware para manejar las rutas de usuarios
+// app.use('/api/usuarios', usuarioRoutes);
+
+// // Middleware para servir archivos estáticos desde la carpeta 'public'
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Middleware para manejar las rutas de autenticación
+// app.use('/auth', authRoutes); // Aquí asegúrate de tener '/auth' como prefijo para todas las rutas de autenticación
+
+// // Middleware para manejar errores
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('Error en el servidor');
+// });
+
+// // Iniciar el servidor
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// });
+/////////////////
 const express = require('express');
 const app = express();
 const usuarioRoutes = require('./routes/usuarioRoutes');
@@ -27,4 +57,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,21 @@
+// const express = require('express');
+// const router = express.Router();
+// const authController = require('../controllers/authController');
+
+// // Ruta para registro de usuario
+// router.post('/register', authController.register);
+
+// // Ruta para inicio de sesión
+// router.post('/login', authController.login);
+
+// module.exports = router;
+
+
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Ruta para registro de usuario
 router.post('/register', authController.register);
-
-// Ruta para inicio de sesión
 router.post('/login', authController.login);
 
 module.exports = router;
