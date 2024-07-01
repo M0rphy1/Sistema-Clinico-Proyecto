e3179c9641c298a62e39f8ed3555afe9a266189c
Gianni Abarca <jefemaestroasf@gmail.com>
2024-07-01 17:01:09 -0500

    limites en numeros

--- src/controllers/medicamentoController.js
+++ src/controllers/medicamentoController.js
@@ -3,8 +3,26 @@
 const Medicamento = require('../models/medicamento');
 const { Op } = require('sequelize');
 
+// Ruta para verificar si el nombre del medicamento ya existe
+exports.verificarNombreMedicamento = async (req, res) => {
+  const { nombre } = req.query;
+
+  try {
+    const medicamentoExistente = await Medicamento.findOne({ where: { nombreMedicamento: nombre } });
+    if (medicamentoExistente) {
+      res.status(400).json({ message: 'El nombre de medicamento ya está en uso. Por favor, intenta con otro.' });
+    } else {
+      res.status(200).json({ message: 'El nombre de medicamento está disponible.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 exports.createMedicamento = async (req, res) => {
   try {
+    if (precio <= 0 || stock <= 0) {
+      return res.status(400).json({ message: 'El precio y el stock deben ser mayores a cero.' });
+    }
     const nuevoMedicamento = await Medicamento.create(req.body);
     res.status(201).json(nuevoMedicamento);
   } catch (error) {
