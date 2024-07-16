0bf80d734d8d5fd1ff6219bc91d65362897f3b4c
Gianni Abarca <jefemaestroasf@gmail.com>
2024-07-16 15:36:50 -0500

    algunos clean

--- NSpotify/authorization_code/app.js
+++ NSpotify/authorization_code/app.js
@@ -14,8 +14,8 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'yourClientIDGoesHere'; // your clientId
-var client_secret = 'YourSecretIDGoesHere'; // Your secret
+var client_id = 'client_id'; // your clientId
+var client_secret = 'secret_client'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 
